76e652a3dc815af3800d3db76219149637faf69b
Sione Tenifa <johntenifa@yahoo.com>
2019-10-05 10:16:08 -0600

    complete

--- main.js
+++ main.js
@@ -1,37 +1,115 @@
 const startButton = document.getElementById('start-btn')
-const questionContainerElememnet = document.getElementById('question-container')
-let shuffledQuestions, currentQuestionIndex
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
-const answerButtonElement = document.getElementById('answer-buttons')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-function startGame() { 
+function startGame() {
   startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
-  questionContainerElememnet.classList.remove('hide')
+  questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
 
 function setNextQuestion() {
+  resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestion(question){
+function showQuestion(question) {
   questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
 
-function selectAnser() {
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
 
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const questions = [
-  { 
-    question: 'what is 2 + 2?',
-    answers:[
-      {text: '4', correct: true},
-      {text: '22', correct: false}
+  {
+    question: 'What is 20 + 2?',
+    answers: [
+      { text: '4', correct: false },
+      { text: '22', correct: true },
+      { text: '56', correct: false },
+      { text: '34', correct: false }
+
+    ]
+  },
+  {
+    question: 'Who is the current president of the USA?',
+    answers: [
+      { text: 'Donald Trump', correct: true },
+      { text: 'George Washington', correct: false },
+      { text: 'Barack Obama', correct: false },
+      { text: 'JFK' , correct: false }
+    ]
+  },
+  {
+    question: 'Who is the best team in the  NBA?',
+    answers: [
+      { text: 'Lakers', correct: false },
+      { text: 'Jazz', correct: true },
+      { text: 'Celtics', correct: false },
+      { text: 'Heat', correct: false }
+    ]
+  },
+  {
+    question: 'What is 90 / 10?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '9', correct: true }
     ]
   }
 ]
