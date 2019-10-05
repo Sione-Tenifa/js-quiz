c9bdf2d797bf637313b0eb84b7e2867d6a348f01
Sione Tenifa <johntenifa@yahoo.com>
2019-10-05 09:48:11 -0600

    questions/array/function

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElememnet = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonElement = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
 
-function startGame() {
-  console.log('Started')
+function startGame() { 
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElememnet.classList.remove('hide')
+  setNextQuestion()
 }
 
 function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+  questionElement.innerText = question.question
 }
 
 function selectAnser() {
 
 }
+
+const questions = [
+  { 
+    question: 'what is 2 + 2?',
+    answers:[
+      {text: '4', correct: true},
+      {text: '22', correct: false}
+    ]
+  }
+]
